4c819dd09cf46323b23a612ef310b34876c3fd3b
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-10-17 01:00:22 -0300

    adding types

--- 01_types/types.js
+++ 01_types/types.js
@@ -1,1 +1,23 @@
 "use strict";
+//string
+var Name = 'João';
+console.info(Name);
+//Number
+var Age = 27;
+console.info(Age);
+Age = 27.6;
+console.info(Age);
+//boolean
+var haveHobbies = false;
+console.info(haveHobbies);
+//tipos explicitos
+var myAge;
+myAge = 27;
+myAge = '27';
+console.info(myAge);
+//array
+var hobbies = ["Play Guitar", "Write Codes"];
+console.info(hobbies);
+//tuplas
+var address = ["Av central", 74];
+console.log(address);
---
+++ 01_types/types.ts
@@ -0,0 +1,30 @@
+//string
+const Name: string = 'João'
+console.info(Name)
+
+
+//Number
+let Age: number = 27
+console.info(Age)
+
+Age = 27.6
+console.info(Age)
+
+
+//boolean
+let haveHobbies:boolean = false
+console.info(haveHobbies)
+
+//tipos explicitos
+let myAge: any
+myAge = 27
+myAge = '27'
+console.info(myAge)
+
+//array
+let hobbies:string[] = ["Play Guitar", "Write Codes"]
+console.info(hobbies)
+
+//tuplas
+const address: [string, number] = ["Av central", 74]
+console.log(address)
